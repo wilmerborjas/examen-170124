c393154191404074a1cf7e30653b72856bc667bb
wilmer Borjas <wilmer.aprendiendo.programacion@gmail.com>
2024-01-17 11:56:21 +0100

    practica 1

---
+++ src/main/java/com/wbg/examen/app/animales/Perro.java
@@ -0,0 +1,35 @@
+package com.wbg.examen.app.animales;
+
+public class Perro {
+
+	private String raza;
+
+	private String color;
+
+	private double peso;
+
+	public String getRaza() {
+		return this.raza;
+	}
+
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPeso() {
+		return this.peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+}
---
+++ src/main/java/com/wbg/examen/app/articulos/Computador.java
@@ -0,0 +1,35 @@
+package com.wbg.examen.app.articulos;
+
+public class Computador {
+
+	private String color;
+
+	private String marca;
+
+	private int pantalla;
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getMarca() {
+		return this.marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public int getPantalla() {
+		return this.pantalla;
+	}
+
+	public void setPantalla(int pantalla) {
+		this.pantalla = pantalla;
+	}
+
+}
